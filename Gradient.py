7bc5591691a421dd59e7ef7a3ed6fe2925e41b93
Andrew Sarracini <andrewsarracini1@gmail.com>
2023-02-21 15:55:58 -0800

    Deleted garbage file

--- Gradient.py
+++ Gradient.py
@@ -49,45 +49,5 @@ Y5 = sem_grades[8]
 
 # plt.show()
 
-''' ----------------------- Copy-Pasting in some Grades code to test out Seaborn 'count plot' ----------------------------------------------'''
 
-my_dict = {'First Year': [8, 3, 6, 0, 8, 6, 5, 4, 3, 6],
-           'Second Year': [9, 10, 11, 11, 8, 9, 8, 8, 9, 3],
-           'Third Year': [4, 8, 7, 8, 11, 7, 9, 11, 11],
-           'Fourth Year': [6, 11, 10, 12, 12, 11, 11, 12],
-           'Fifth Year': [12, 10, 11, 11],
-           'Postbac': [12]}
-
-grades_df = pd.DataFrame({key: pd.Series(value)
-                         for key, value in my_dict.items()})
-
-# all_avg = grades_df.mean()
-
-# fir_avg = grades_df['First Year'].mean()
-# sec_avg = grades_df['Second Year'].mean()
-# thir_avg = grades_df['Third Year'].mean()
-# four_avg = grades_df['Fourth Year'].mean()
-# fif_avg = grades_df['Fifth Year'].mean()
-
-# total_avg = (grades_df['First Year'].sum() + grades_df['Second Year'].sum() + grades_df['Third Year'].sum() +
-#              grades_df['Fourth Year'].sum() + grades_df['Fifth Year'].sum() + grades_df['Postbac'].sum()) / 42
-
-''' Separating the data by semester, 2D array '''
-sem_grades = [[8, 3, 6, 0, 8],
-              [6, 5, 4, 3, 6],
-              [9, 10, 11, 11, 8],
-              [9, 8, 8, 9, 3],
-              [4, 8, 7, 8, 11],
-              [7, 9, 11, 11],
-              [6, 11, 10, 12],
-              [12, 11, 11, 12],
-              [12, 10, 11, 11, 12]]
-
-sem_avgs = [np.average(sem) for sem in sem_grades]
-sem_sum = [sum(grade) for grade in sem_grades]
-
-df = pd.DataFrame(sem_grades)
-print(df)
-
-sns.countplot(df)
-plt.show()
+''' ---------------------------- Testing out Seaborn ---------------------'''
